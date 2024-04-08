ba200cda1abd69060c0d71cf820a74745d981822
Pauline Hesse <pipelettelive@gmail.com>
2024-04-08 11:33:18 +0200

    Modif month problem  8/4/2024

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -20,6 +20,7 @@ const Slider = () => {
   useEffect(() => {
     nextCard();
   });
+  
 return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
@@ -43,7 +44,7 @@ return (
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}-${radioIdx}`}
+                  key={event.id}
                   type="radio"
                   name="radio-button"
                   checked={index === radioIdx}
